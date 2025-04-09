c7f1753bb4e9f14b2ecac2d765c5e17f2b5ddbf5
5-12 <hellowordare@163.com>
2025-04-09 18:05:24 +0800

    empty-zeros

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 import numpy as np
-dt=np.dtype([("n1","i4"),
-             ("n2","i4")
-             ])
-test=np.array([[(1 , 2 ),( 3 , 4 )],[( 5 , 6 ),( 7 , 8 )]],dtype=dt)
-# np.ndarray
-print(test.itemsize)
-print(test.ndim)
-print(test.shape)
-print(test)
+dt=np.dtype([("n1","i1")])
+toend=np.empty((4,2),dtype=dt)
+print(toend)
 print()
-test1=test.reshape((4,1))
-print(test1.itemsize)
-print(test1.ndim)
-print(test1.shape)
-print(test1)
+toend=np.zeros((13,13),dtype=dt,)
+print(toend)
