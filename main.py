364ccb54392cbcaebef8fb142dde2f8f39241a62
5-12 <hellowordare@163.com>
2025-04-10 17:54:28 +0800

    TestTorchFourRules

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import torch 
-test=torch.tensor([[1,2],[2,3]])
-print(test)
-print()
-test1=torch.zeros(3,3)
-print(test1)
-print()
-test2=torch.rand(3,3,4)
-print(test2)
-print()
-test3=torch.eye(5)
-print(test3)
+test=torch.tensor([2,3])
+test1=torch.tensor([2,3])
+test_copy=test
+test_end=test.add(test1)
+test_end1=test*test1
+test_copy.add_(test1)
+print(test_end)
+print(test_copy)
+print(test_end1)
