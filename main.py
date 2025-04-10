06a3236c21b2013ed3f5e241a1444b668f1a1ef6
5-12 <hellowordare@163.com>
2025-04-10 18:10:13 +0800

    at

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import torch 
-test=torch.tensor([2,3])
-test1=torch.tensor([2,3])
-test_copy=test
-test_end=test.add(test1)
-test_end1=test*test1
-test_copy.add_(test1)
+test=torch.rand(3)
+test1=torch.rand(3,2,3,1)
+test_end=test@test1
 print(test_end)
-print(test_copy)
-print(test_end1)
